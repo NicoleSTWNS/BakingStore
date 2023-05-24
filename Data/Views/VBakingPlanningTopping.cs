ac9aab0336a97a51f9949b1113cd6758f6549252
NiKo <nicolassetiawan2@gmail.com>
2023-05-24 13:09:58 +0700

    Change niko

---
+++ Data/Views/VBakingPlanningTopping.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakingStore.Data.Views;
+
+public class VBakingPlanningTopping
+{
+	public int Id { get; set; }
+	public int BakingPlanId { get; set; }
+	public int ToppingId { get; set; }
+	public int Qty { get; set; }
+	public string ToppingName { get; set; }
+	public string ToppingUomCode { get; set; }
+	public float ToppingAvailableStock { get; set; }
+}
